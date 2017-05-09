7b63210c5f8da5bea43c09435f2075a911c5af8f
Brendon <btucker1992@gmail.com>
2017-05-09 16:34:27 +0800

    passed the test

--- Course1/week2/efficientFib.py
+++ Course1/week2/efficientFib.py
@@ -6,17 +6,20 @@ Created on Tue May  9 14:57:34 2017
 @author: brendontucker
 """
 n = int(input())
-n = 13
+#n = 2
 resultList = [0, 1]
 result = 0
-if n <= 2:
-    result = resultList[n-1]
+if n == 0:
+    result = 0
+elif n == 1:
+    result = 1
 else:
+    n += 1
     for x in range(2, n):
         newNum = resultList[x-2] + resultList[x-1]
         resultList.append(newNum)
     result = resultList[-1]
     
 print(result)
-        
+
 
