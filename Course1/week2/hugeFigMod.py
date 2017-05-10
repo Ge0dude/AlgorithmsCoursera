3692135459d758eb3b2e990057d40ec99d8d118e
Brendon <btucker1992@gmail.com>
2017-05-10 14:10:52 +0800

    going to have to do some kind of automated exploration to understand growth better... or could just googld pisano growth

---
+++ Course1/week2/hugeFigMod.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 10 13:57:20 2017
+
+@author: brendontucker
+
+mainly an exploration file to understand problem growth not to implement an 
+efficient solution
+modVar-- 2, 3, 4, 5, 6
+length-- 4, 8, 5, 20, 24 
+"""
+
+#n = int(input())
+n = 30
+modVar = 6
+resultList = [0, 1]
+modVarList = [0, 1]
+result = 0
+if n == 0:
+    result = 0
+elif n == 1:
+    result = 1
+else:
+    n += 1
+    for x in range(2, n):
+        newNum = resultList[x-2] + resultList[x-1]
+        resultList.append(newNum)
+        modNum = newNum % modVar
+        modVarList.append(modNum)
+
+        
+        
