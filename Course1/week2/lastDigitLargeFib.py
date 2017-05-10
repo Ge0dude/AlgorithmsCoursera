8a62c0ff3ee0ca7e700eb80b581f968f35b2c210
Brendon <btucker1992@gmail.com>
2017-05-10 08:30:50 +0800

    tried to fix the memory issues

--- Course1/week2/lastDigitLargeFib.py
+++ Course1/week2/lastDigitLargeFib.py
@@ -6,8 +6,8 @@ Created on Wed May 10 07:46:15 2017
 @author: brendontucker
 """
 
-#n = int(input())
-n = 200
+n = int(input())
+#n = 200
 resultList = [0, 1]
 result = 0
 if n == 0:
@@ -17,8 +17,10 @@ elif n == 1:
 else:
     n += 1
     for x in range(2, n):
-        newNum = resultList[x-2] + resultList[x-1]
+        newNum = resultList[0] + resultList[1]
         resultList.append(newNum)
+        resultList.pop(0)
+        #can remove something from front here to save memory 
     result = resultList[-1]
     
 result = str(result)
