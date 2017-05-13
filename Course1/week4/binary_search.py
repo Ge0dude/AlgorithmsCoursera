9050b7a22883bf742523e5cf910165047f16ac7a
Brendon <btucker1992@gmail.com>
2017-05-13 19:03:54 +0800

    need to implement the four argument option

--- Course1/week4/binary_search.py
+++ Course1/week4/binary_search.py
@@ -1,18 +1,33 @@
 # Uses python3
+'''
+changed x to b so that it matched the problem explanation
+'''
 import sys
 
-file = open('/Users/brendontucker/AlgorithmsCoursera/Course1/week4/binarySearchTest1')
-sys.stdin = file
+#file = open('/Users/brendontucker/AlgorithmsCoursera/Course1/week4/binarySearchTest1')
+#sys.stdin = file
 
-def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
 
-def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
-            return i
-    return -1
+def binary_search_recursive(list1, toFind):
+    if len(list1) == 0:
+        return -1
+    elif toFind < list1[0]:
+        return -1
+    elif toFind > list1[len(list1) -1]:
+        return -1
+    else:
+        mid = len(list1)//2
+        if toFind == list1[mid]:
+            return mid
+        elif toFind > list1[mid]:
+            return binary_search_recursive(list1[mid+1:], toFind)
+        else:
+            return binary_search_recursive(list1[:mid], toFind)
+
+#slicing inceases complexity... might need four arguments
+
+
+            
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -20,6 +35,59 @@ if __name__ == '__main__':
     n = data[0]
     m = data[n + 1]
     a = data[1 : n + 1]
-    for x in data[n + 2:]:
+    for b in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search_recursive(a, b), end = ' ')
+     
+      
+#def linear_search(a, b):
+#    for i in range(len(a)):
+#        if a[i] == b:
+#            return i
+#    return -1
+
+
+
+#
+#def binary_search(listA, toFind):
+#    left, right = 0, len(a) - 1
+#    # write your code here
+#    low = listA[left]
+#    print(low)
+#    high = listA[right]
+#    print(high)
+#    if toFind > high:
+#        return -1
+#    elif toFind < low:
+#        return -1
+#    else:
+#        mid = left + ((right - left) // 2)
+#        print(mid)
+#        if a[mid] == b:
+#            return mid
+#        elif b < a[mid]:
+#            return binary_search(listA[:mid], toFind)
+#        elif b > a[mid]:
+#            return binary_search(listA[:right], toFind)
+
+
+#def binary_search(listA, toFind, left=0, right=1):
+#    left, right = 0, len(a) - 1
+#    # write your code here
+#    low = listA[left]
+#    print(low)
+#    high = listA[right]
+#    print(high)
+#    if toFind > high:
+#        return -1
+#    elif toFind < low:
+#        return -1
+#    else:
+#        mid = left + ((right - left) // 2)
+#        print(mid)
+#        if a[mid] == b:
+#            return mid
+#        elif b < a[mid]:
+#            return binary_search(listA, toFind, left, mid)
+#        elif b > a[mid]:
+#            return binary_search(listA, toFind, mid, right)
