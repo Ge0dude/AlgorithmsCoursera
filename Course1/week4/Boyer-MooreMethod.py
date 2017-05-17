f7441042e03a6b8927bdff7ef1486fd8fef76dcc
Brendon <btucker1992@gmail.com>
2017-05-17 11:35:30 +0800

    got it! used an elif when i needed an if...

--- Course1/week4/Boyer-MooreMethod.py
+++ Course1/week4/Boyer-MooreMethod.py
@@ -10,36 +10,23 @@ is treated
 """
 import sys
 
-#file = open('/Users/brendontucker/AlgorithmsCoursera/Course1/week4/majorityTest1')
+#file = open('/Users/brendontucker/AlgorithmsCoursera/Course1/week4/majorityTest2')
 #sys.stdin = file
 
 def get_majority_element(array1, length):
-    m = 0 #element
-    i = 0 #counter
-    if len(array1) == 1:
-        return 1    
-    for x in a:
-        if m == 0:
-            m = x
-            #i = 1
-        elif m == x:
-            i += 1
-        else:
-            i -= 1
-            
-#    candidate = 0
-#    count = 0
-#    for value in input:
-#      if count == 0:
-#        candidate = value
-#      if candidate == value:
-#        count += 1
-#      else:
-#        count -= 1
+    candidate = 0
+    count = 0
+    for value in array1:
+      if count == 0:
+        candidate = value
+      if candidate == value:
+        count += 1
+      else:
+        count -= 1
     
     j = 0        
-    for y in a:
-        if y == m:
+    for values in a:
+        if values == candidate:
             j += 1
     
     ans = 0        
@@ -55,28 +42,6 @@ if __name__ == '__main__':
     n, *a = list(map(int, input.split()))
     print(get_majority_element(a, n))
 
-'''global version for testing'''
 
-#input = sys.stdin.read()
-#n, *a = list(map(int, input.split()))
-#
-#m = 0 #element
-#i = 0 #counter
-#for x in a:
-#    if m == 0:
-#        m = x
-#        i = 1
-#    elif m == x:
-#        i += 1
-#    else:
-#        i -= 1
-#
-#j = 0        
-#for y in a:
-#    if y == m:
-#        j += 1
-#
-#ans = 0        
-#if j/n > 1/2:
-#    ans = m 
-#        
+
+  
--- Course1/week4/Boyer-MooreScripting.py
+++ Course1/week4/Boyer-MooreScripting.py
@@ -8,6 +8,40 @@ Created on Wed May 17 11:04:42 2017
 
 import sys
 
-#file = open('/Users/brendontucker/AlgorithmsCoursera/Course1/week4/majorityTest1')
-#sys.stdin = file
+file = open('/Users/brendontucker/AlgorithmsCoursera/Course1/week4/majorityTest1')
+sys.stdin = file
 
+input = sys.stdin.read()
+n, *a = list(map(int, input.split()))
+
+candidate = 0
+count = 0
+for value in a:
+  if count == 0:
+    candidate = value
+  if candidate == value:
+    count += 1
+  else:
+    count -= 1
+
+j = 0        
+for values in a:
+    if values == candidate:
+        j += 1
+
+ans = 0        
+if j/n > 1/2:
+    ans = 1 
+      
+    
+    
+    
+candidate = 0
+count = 0
+for value in input:
+  if count == 0:
+    candidate = value
+  if candidate == value:
+    count += 1
+  else:
+    count -= 1
