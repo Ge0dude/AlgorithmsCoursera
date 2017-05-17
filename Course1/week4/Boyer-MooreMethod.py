3436cd83c44a70043b6be08b5200b5dd356643e4
Brendon <btucker1992@gmail.com>
2017-05-17 11:15:00 +0800

    changed the input parameters... still can't seem to pass test 5

--- Course1/week4/Boyer-MooreMethod.py
+++ Course1/week4/Boyer-MooreMethod.py
@@ -4,23 +4,38 @@
 Created on Tue May 16 16:00:32 2017
 
 @author: brendontucker
+
+error is likely whether I initialize i = 1 upon changing m, and then how j
+is treated 
 """
 import sys
 
 #file = open('/Users/brendontucker/AlgorithmsCoursera/Course1/week4/majorityTest1')
 #sys.stdin = file
 
-def get_majority_element(array1):
+def get_majority_element(array1, length):
     m = 0 #element
     i = 0 #counter
+    if len(array1) == 1:
+        return 1    
     for x in a:
         if m == 0:
             m = x
-            i = 1
+            #i = 1
         elif m == x:
             i += 1
         else:
             i -= 1
+            
+#    candidate = 0
+#    count = 0
+#    for value in input:
+#      if count == 0:
+#        candidate = value
+#      if candidate == value:
+#        count += 1
+#      else:
+#        count -= 1
     
     j = 0        
     for y in a:
@@ -28,7 +43,7 @@ def get_majority_element(array1):
             j += 1
     
     ans = 0        
-    if j/n >= 1/2:
+    if j/length > 1/2:
         ans = 1
         
     return ans
@@ -38,7 +53,7 @@ def get_majority_element(array1):
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    print(get_majority_element(a))
+    print(get_majority_element(a, n))
 
 '''global version for testing'''
 
---
+++ Course1/week4/Boyer-MooreScripting.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 17 11:04:42 2017
+
+@author: brendontucker
+"""
+
+import sys
+
+#file = open('/Users/brendontucker/AlgorithmsCoursera/Course1/week4/majorityTest1')
+#sys.stdin = file
+
