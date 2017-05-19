6869a9743de17c7b856637ec14056c0d05948c54
Brendon <btucker1992@gmail.com>
2017-05-20 07:53:14 +0800

    starting usign debugging practices to figure out what is going on here--think I'm getting a handle on it

--- Course1/week5/coinChanging.py
+++ Course1/week5/coinChanging.py
@@ -6,15 +6,29 @@ Created on Fri May 19 17:41:48 2017
 @author: brendontucker
 
 using this as an example to better understand dynamic programming
+lets do some debugging with print statements
 """
 
 coinValueList = [1, 5, 21, 25]
 change = 63
 minCoins = [0 for x in range(change + 1)]
 
-for cents in range(10):#(change+1):
-      coinCount = cents
-      for j in [c for c in coinValueList if c <= cents]:
-            if minCoins[cents-j] + 1 < coinCount:
-               coinCount = minCoins[cents-j]+1
-      minCoins[cents] = coinCount
+for cents in range(30):#(change+1):
+    print('START LOOP')
+    print('_________')
+    print('cents is:', cents)
+    coinCount = cents
+    print('so is coinCount:', coinCount)
+    for j in [c for c in coinValueList if c <= cents]:
+        print('j is:', j)
+        if minCoins[cents-j] + 1 < coinCount:
+             coinCount = minCoins[cents-j] + 1
+             print('coinCount is now:', coinCount)
+    minCoins[cents] = coinCount
+    print('the answer is:', coinCount)
+      
+#need to understand this line better
+
+#[c for c in coinValueList if c <= cents]
+'''creates a list of every coin <= cents'''
+
